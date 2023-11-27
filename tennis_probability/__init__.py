e257d5064ca7d06c6e4a60e5c75324e9af8ef5e6
Max Larsson <maxslarsson@gmail.com>
2023-11-27 17:18:58 -0500

    Remove importlib_metadata

--- tennis_probability/__init__.py
+++ tennis_probability/__init__.py
@@ -1,13 +1,3 @@
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata  # type: ignore
-
-try:
-    __version__ = importlib_metadata.version(__name__)
-except importlib_metadata.PackageNotFoundError:
-    __version__ = ""
-
 from .find_p import find_p
 from .set import set
 from .game import game
